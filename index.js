df209e5de040369a6baa1c8e5e3f2fd8e616da57
Tapos Chandro <taposchandro231@gmail.com>
2024-06-06 20:20:28 +0600

    cors orgin added

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,")
+    next();
+  });
+
 
 
 
